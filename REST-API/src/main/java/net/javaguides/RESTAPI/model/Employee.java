f331389fd01775981dc3165c4cccfbd9b810bbeb
Nabil1907 <snabil084@gmail.com>
2022-08-07 13:06:43 +0200

    Add Project first time

---
+++ REST-API/src/main/java/net/javaguides/RESTAPI/model/Employee.java
@@ -0,0 +1,30 @@
+package net.javaguides.RESTAPI.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor // to create an object first (uses no arg-constructor)
+@AllArgsConstructor // to create object by passing all parameters
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+
+}
